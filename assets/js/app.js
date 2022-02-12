74368a2e1395e2b3eaf4df36a5522e7ef8f2aa3a
Jay B <79182784+JayBee-K@users.noreply.github.com>
2022-02-12 15:54:34 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,39 @@
 let windowWidth = $(window).width();
 const handleTouchMove = function (ev) {
-	ev.preventDefault();
+    ev.preventDefault();
 }
 
+const partnerSlide = function () {
+    new Swiper('#partner-slide', {
+        speed: 1000,
+        spaceBetween: 15,
+        autoplay: {
+            delay: 10000,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1.5,
+            },
+            600: {
+                slidesPerView: 2.5,
+            },
+            768: {
+                slidesPerView: 3.5,
+            },
+            991: {
+                slidesPerView: 4.5,
+            },
+            1024: {
+                slidesPerView: 5.5,
+            },
+            1281: {
+                slidesPerView: 6.5,
+            }
+        }
+    });
+}
 
 $(function () {
+    partnerSlide();
 });
